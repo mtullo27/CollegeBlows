3dda1e1ba99376535f9f33ff31079ab8b0d13401
mtullo27 <mtullo1@binghamton.edu>
2021-04-16 10:24:39 -0400

    started character creation

---
+++ app/src/main/java/com/example/collegeblows/CharacterCreation.java
@@ -0,0 +1,58 @@
+package com.example.collegeblows;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class CharacterCreation extends AppCompatActivity {
+
+    public final static int PICK_PHOTO_CODE = 1046;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_creation);
+    }
+
+    public void onPickPhoto(View v){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if(intent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+    }
+
+    public Bitmap loadFromUri(Uri photoUri){
+        Bitmap image = null;
+        try {
+            if(Build.VERSION.SDK_INT > 27){
+                ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            }
+            else image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ((data != null) && requestCode == PICK_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+            Bitmap selectedImage = loadFromUri(photoUri);
+            ImageView ivPreview = (ImageView) findViewById(R.id.Player);
+            ivPreview.setImageBitmap(selectedImage);
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
