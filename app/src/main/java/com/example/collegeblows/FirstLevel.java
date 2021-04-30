3897cf158c8cd4df155012ba43bd5530e3c3b025
llasurd1 <llasurdo@gmail.com>
2021-04-30 10:01:11 -0400

    new

--- app/src/main/java/com/example/collegeblows/FirstLevel.java
+++ app/src/main/java/com/example/collegeblows/FirstLevel.java
@@ -1,4 +1,27 @@
 package com.example.collegeblows;
 
-public class FirstLevel {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstLevel extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("OnCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstlevel_main);
+
+    }
+
+
+    public void instructionClick(View view) {
+        System.out.println("Hello");
+        Intent intent =  new Intent(this, Instruction2.class);
+        System.out.println("After Intent");
+        startActivity(intent);
+        System.out.println("Last");
+    }
 }
