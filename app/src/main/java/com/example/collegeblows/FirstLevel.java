47408b06e7da67fdd9c96cba4d6bf5e316d6c200
llasurd1 <llasurdo@gmail.com>
2021-05-03 10:11:12 -0400

    new

--- app/src/main/java/com/example/collegeblows/FirstLevel.java
+++ app/src/main/java/com/example/collegeblows/FirstLevel.java
@@ -6,22 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FirstLevel extends AppCompatActivity {
+    Button b_option1, b_option2;
+    TextView text;
+    int step;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         System.out.println("OnCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_level);
-
+        b_option1 = findViewById(R.id.button_option1);
+        b_option2 = findViewById(R.id.button_option2);
+        text = findViewById(R.id.text_view);
+        step = 0;
     }
 
 
-    public void instructionClick(View view) {
-        System.out.println("Hello");
-        Intent intent =  new Intent(this, Instruction2.class);
-        System.out.println("After Intent");
-        startActivity(intent);
-        System.out.println("Last");
+    public void option1Click(View view) {
+        if(step==0) {
+            b_option1.setText("option1 has changed");
+            step++;
+        }
+    }
+    public void option2Click(View view) {
+        if(step==0) {
+            step++;
+        }
     }
 }
