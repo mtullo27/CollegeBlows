3e9ab4ec42d678c29af55d9772979c6b419490d0
llasurd1 <llasurdo@gmail.com>
2021-05-18 18:24:13 -0400

    demo

--- app/src/main/java/com/example/collegeblows/Level1.java
+++ app/src/main/java/com/example/collegeblows/Level1.java
@@ -39,47 +39,242 @@ public class Level1 extends AppCompatActivity {
     public void option1Click(View view) {
         if(step==0) {
             hp.setText("" + health);
-            text.setText("Some options will change you health");
-            b_option1.setText("Lose health");
-            b_option2.setText("Gain health");
+            text.setText("You wake up feeling hungry, what do you want to eat?");
+            b_option1.setText("A healthy breakfast");
+            b_option2.setText("A dining hall muffin");
             step++;
         }
         else if(step==1) {
-            updateHealth(-1);
-            text.setText("If you reach 0 you lose");
-            b_option1.setText("Lose more health");
-            b_option2.setText("Gain more health");
+            updateHealth(+2);
+            text.setText("Now that you've eaten, its time to start the day");
+            b_option1.setText("Grab a coat");
+            b_option2.setText("Who needs a coat");
             step++;
         }
         else if(step==2) {
-            updateHealth(-9);
-            text.setText("If you reach 0 you lose");
-            b_option1.setText("Lose more health");
-            b_option2.setText("Gain more health");
+            updateHealth(+3);
+            text.setText("On your way out you notice your roommate hasn't done his dishes");
+            b_option1.setText("Ask him nicely to do them");
+            b_option2.setText("Intimidate him into doing them");
+            step++;
+        }
+        else if(step==3) {
+            if(charisma_check==true) {
+                text.setText("Your roommate happily says he'll do them, time to go to class");
+                b_option1.setText("Walk there");
+                b_option2.setText("Take the bus");
+            }
+            else {
+                updateHealth(-1);
+                text.setText("Your roommate slaps you and walks away, time to go to class");
+                b_option1.setText("Walk there");
+                b_option2.setText("Take the bus");
+            }
+            step++;
+        }
+        else if(step==4) {
+            text.setText("Oh no, you forgot you had a test today and didn't study");
+            b_option1.setText("Do your best on the test");
+            b_option2.setText("Try to cheat off the guy next to you");
+            step++;
+        }
+        else if(step==5) {
+            if(int_check==true) {
+                text.setText("The test was easy for you so you leave");
+                b_option1.setText("Walk away happy");
+                b_option2.setText("Walk away unbothered");
+            }
+            else {
+                updateHealth(-5);
+                text.setText("That test whooped your ass and you leave");
+                b_option1.setText("Walk away sad");
+                b_option2.setText("Walk away unbothered");
+            }
+            step++;
+        }
+        else if(step==6) {
+            text.setText("You remember you're being kicked out of your dorm and decide to go confront Harvey");
+            b_option1.setText("Walk there");
+            b_option2.setText("Take the bus");
+            step++;
+        }
+        else if(step==7) {
+            text.setText("You enter Harvey's office and he asks you to sit down");
+            b_option1.setText("Sit down");
+            b_option2.setText("Keep standing");
+            step++;
+        }
+        else if(step==8) {
+            text.setText("Harvey make you an offer, if you can beat him in a duel you can have your room");
+            b_option1.setText("Accept");
+            b_option2.setText("Accept but the second option");
+            step++;
+        }
+        else if(step==9) {
+            text.setText("Harvey quickly throws his chair at you");
+            b_option1.setText("Try to dodge it");
+            b_option2.setText("Block the chair with your arms");
+            step++;
+        }
+        else if(step==10) {
+            if(const_check==true) {
+                text.setText("You quickly dodge the chair and its time to strike back");
+                b_option1.setText("Throw your own chair");
+                b_option2.setText("Throw a punch at him");
+            }
+            else {
+                updateHealth(-4);
+                text.setText("The chair smacks into your head, but its time to strike back");
+                b_option1.setText("Throw your own chair");
+                b_option2.setText("Throw a punch at him");
+            }
+            step++;
+        }
+        else if(step==11) {
+            text.setText("Harvey is knocked down from your attack, but tries to sweep your leg");
+            b_option1.setText("Limbo under it");
+            b_option2.setText("Jump over it");
+            step++;
+        }
+        else if(step==12) {
+            text.setText("You dodge his attack");
+            b_option1.setText("Stand over him triumphantly");
+            b_option2.setText("Grab a pen to defend yourself");
+            step++;
+        }
+        else if(step==13) {
+            text.setText("Fine you win, Harvey says, you can have your room back");
+            b_option1.setText("Thank him");
+            b_option2.setText("Leave without saying anything");
+            step++;
+        }
+        else if(step==14) {
+            text.setText("Congrats you win");
+            b_option1.setText("good job!");
+            b_option2.setText("you did well");
             step++;
         }
     }
     public void option2Click(View view) {
         if(step==0) {
             hp.setText("" + health);
-            text.setText("Some options will change you health");
-            b_option1.setText("Lose health");
-            b_option2.setText("Gain health");
+            text.setText("You wake up feeling hungry, what do you want to eat?");
+            b_option1.setText("A healthy breakfast");
+            b_option2.setText("A dining hall muffin");
             step++;
         }
         else if(step==1) {
-            updateHealth(+1);
-            text.setText("If you reach 0 you lose");
-            b_option1.setText("Lose more health");
-            b_option2.setText("Gain more health");
+            updateHealth(+5);
+            text.setText("Now that you've eaten, its time to start the day");
+            b_option1.setText("Grab a coat");
+            b_option2.setText("Who needs a coat");
             step++;
         }
         else if(step==2) {
-            updateHealth(+9);
-            text.setText("If you reach 0 you lose");
-            b_option1.setText("Lose more health");
-            b_option2.setText("Gain more health");
+            updateHealth(+5);
+            text.setText("On your way out you notice your roommate hasn't done his dishes");
+            b_option1.setText("Ask him nicely to do them");
+            b_option2.setText("Intimidate him into doing them");
             step++;
         }
+        else if(step==3) {
+            if(strength_check==true) {
+                text.setText("Your roommate shivering, says he'll do them, time to go to class");
+                b_option1.setText("Walk there");
+                b_option2.setText("Take the bus");
+            }
+            else {
+                updateHealth(-1);
+                text.setText("Your roommate slaps you and walks away, time to go to class");
+                b_option1.setText("Walk there");
+                b_option2.setText("Take the bus");
+            }
+            step++;
+        }
+        else if(step==4) {
+            text.setText("Oh no, you forgot you had a test today and didn't study");
+            b_option1.setText("Do your best on the test");
+            b_option2.setText("Try to cheat off the guy next to you");
+            step++;
+        }
+        else if(step==5) {
+            if(dex_check==true) {
+                text.setText("The kid next to you left his paper wide open and the test was easy");
+                b_option1.setText("Walk away happy");
+                b_option2.setText("Walk away unbothered");
+            }
+            else {
+                updateHealth(-10);
+                text.setText("The teacher caught you and kicked you out");
+                b_option1.setText("Walk away sad");
+                b_option2.setText("Walk away unbothered");
+            }
+            step++;
+        }
+        else if(step==6) {
+            text.setText("You remember you're being kicked out of your dorm and decide to go confront Harvey");
+            b_option1.setText("Walk there");
+            b_option2.setText("Take the bus");
+            step++;
+        }
+        else if(step==7) {
+            text.setText("You enter Harvey's office and he asks you to sit down");
+            b_option1.setText("Sit down");
+            b_option2.setText("Keep standing");
+            step++;
+        }
+        else if(step==8) {
+            text.setText("Harvey make you an offer, if you can beat him in a duel you can have your room");
+            b_option1.setText("Accept");
+            b_option2.setText("Accept but the second option");
+            step++;
+        }
+        else if(step==9) {
+            text.setText("Harvey quickly throws his chair at you");
+            b_option1.setText("Try to dodge it");
+            b_option2.setText("Block the chair with your arms");
+            step++;
+        }
+        else if(step==10) {
+            if(const_check==true) {
+                text.setText("The chair shatters on your arms and its time to strike back");
+                b_option1.setText("Throw your own chair");
+                b_option2.setText("Throw a punch at him");
+            }
+            else {
+                updateHealth(-4);
+                text.setText("The chair smacks into your head, but its time to strike back");
+                b_option1.setText("Throw your own chair");
+                b_option2.setText("Throw a punch at him");
+            }
+            step++;
+        }
+        else if(step==11) {
+            text.setText("Harvey is knocked down from your attack, but tries to sweep your leg");
+            b_option1.setText("Limbo under it");
+            b_option2.setText("Jump over it");
+            step++;
+        }
+        else if(step==12) {
+            updateHealth(-5);
+            text.setText("You get knocked over but get back up");
+            b_option1.setText("Stand over him triumphantly");
+            b_option2.setText("Grab a pen to defend yourself");
+            step++;
+        }
+        else if(step==13) {
+
+            text.setText("Fine you win, Harvey says, you can have your room back");
+            b_option1.setText("Thank him");
+            b_option2.setText("Leave without saying anything");
+            step++;
+        }
+        else if(step==14) {
+            text.setText("Congrats you win");
+            b_option1.setText("good job!");
+            b_option2.setText("you did well");
+            step++;
+        }
+
     }
 }
